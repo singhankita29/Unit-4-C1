932bf88f7b2f08990832e7e9d9263c804d20269f
singhankita29 <100141535+singhankita29@users.noreply.github.com>
2022-05-23 17:03:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React,{useState} from "react";
+import TaskApp from "./components/TaskApp";
 
 function App() {
-  return <div>{/* Code Here */}</div>;
+const[inputList,setInputList] = useState("");
+const [Items,setItems] =useState([]);
+
+const itemEvent = (event) => {
+  setInputList(event.target.value);
+};
+const listOfItems=() =>{
+ setItems((oldItems)=>{
+   return [...oldItems,inputList];
+ });
+ setInputList("");
+};
+
+const deleteItem = (id) => {
+  console.log('delete');
+
+  setItems((oldItems)=>{
+    return oldItems.filter((arrElem,index) =>{
+      return index !== id;
+    });
+  });
+};
+  return <div>{/* Code Here */
+  <div className="main_div">
+    <div className="center_div>">
+      <br/>
+      <h1>Todo List</h1>
+      <br/>
+      <input type="text" 
+      placeholder="Add task" 
+      onChange={itemEvent}/>
+
+      <button onClick={listOfItems}> + </button>
+      <ol>
+        {/* <li>{inputList}</li>*/}
+       {
+          Items.map( (itemvalue,index) =>{
+           return <TaskApp 
+            key={index}
+            id={index} 
+            text={itemvalue}
+            onSelect={deleteItem}/>
+          })
+       }
+      </ol>
+      </div>
+    </div>
+  }
+  </div>;
 }
 
 export default App;
--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-
+import remove from "../assets/remove.svg"
 import styles from "./taskApp.module.css";
 
-const TaskApp = () => {
+const TaskApp = (props) => {
   // NOTE: do not delete `data-cy` key value pair
+  
   return (
     <div data-cy="task-app" className={styles.taskApp}>
       {/* Header */}
       {/* Add Task */}
       {/* Tasks */}
+     
+       <>
+       <div className="todo_style">
+         <i className="circle" src={remove} 
+         onClick={props.onSelect}/>
+       <li>{props.text}</li>
+       </div>
+       </>
+    
+      
     </div>
   );
 };
